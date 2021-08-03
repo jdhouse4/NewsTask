5bc82164dd7ed677191f3e7ec3b5c571278ae899
Russell Gordon <rgordon@lcs.on.ca>
2021-08-03 10:56:59 -0400

    Got the main list styled and working.

--- NewsTask/Views/ContentView.swift
+++ NewsTask/Views/ContentView.swift
@@ -16,9 +16,55 @@ struct ContentView: View {
         NavigationView {
             
             List(newsItems) { item in
-                Text(item.title)
+                
+                Link(destination: URL(string: item.url)!) {
+                    ZStack {
+                        
+                        // See: https://swiftwithmajid.com/2021/07/07/mastering-asyncimage-in-swiftui/
+                        AsyncImage(url: URL(string: item.main_image)!) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .padding(0)
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 300, height: 200)
+                            .background(Color.gray)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                            .padding(0)
+
+                        VStack {
+                            Spacer()
+                            VStack {
+                                HStack {
+                                    Spacer(minLength: 50)
+                                    Text(item.title)
+                                        .bold()
+                                        .multilineTextAlignment(.center)
+                                        .padding(.top, 5)
+                                    Spacer(minLength: 50)
+                                }
+                                HStack {
+                                    Spacer(minLength: 50)
+                                    Text(item.strap)
+                                        .font(.subheadline)
+                                        .multilineTextAlignment(.center)
+                                        .padding(.bottom, 5)
+                                    Spacer(minLength: 50)
+                                }
+                            }
+                            .background(Color.primary.colorInvert().opacity(0.8))
+                        }
+                        
+                    }
+
+                }
+                .listRowSeparator(.hidden)
+                
             }
-            .navigationTitle("News")
+            .listStyle(PlainListStyle())
+            .navigationTitle("Articles")
             .task {
                 
                 do {
@@ -31,15 +77,25 @@ struct ContentView: View {
                                 
                                 let url = URL(string: "https://www.hackingwithswift.com/samples/news-\(i).json")!
                                 
+                                // Get the data
                                 let (data, _) = try await URLSession.shared.data(from: url)
                                 
+                                // Just in case task was cancelled
+                                try Task.checkCancellation()
+                                
+                                // Decode the data from JSON into Swift structures
                                 return try JSONDecoder().decode([NewsItem].self, from: data)
                                 
                             }
                                                         
                         }
                         
-                        // Collapse the news items down into one file
+                        // Alt: Put the results into the array as soon as they are ready
+//                        for try await result in group {
+//                            newsItems.append(contentsOf: result)
+//                        }
+                        
+                        // Collapse the news items down into one file (this puts all the results all at once)
                         let allNewsItems = try await group.reduce(into: [NewsItem]()) {
                             $0 += $1
                         }
