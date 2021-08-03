3d37dcc84d1f134ad0df062fd6ec6b71f70ee01c
Russell Gordon <rgordon@lcs.on.ca>
2021-08-03 10:10:11 -0400

    Initial Commit

---
+++ NewsTask/NewsTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTaskApp.swift
+//  NewsTask
+//
+//  Created by Russell Gordon on 2021-08-03.
+//
+
+import SwiftUI
+
+@main
+struct NewsTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
